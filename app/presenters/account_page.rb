a1ba7755d17deccaf80266fc11a7e4b1aec8d808
Rob Whittaker <cyclistsdismount@gmail.com>
2016-11-01 12:12:47 +0000

    Refactor AccountPage presenter

--- app/presenters/account_page.rb
+++ app/presenters/account_page.rb
@@ -1,22 +1,33 @@
 class AccountPage
-  include ActionView::Helpers::NumberHelper
+  def initialize(user)
+    @user = user
+  end
 
-  attr_reader :billable_email, :repos, :subscription
-
-  def initialize(repos:, billable_email:, subscription:)
-    @billable_email = billable_email
-    @repos = repos
-    @subscription = subscription
+  def billable_email
+    user.billable_email
   end
 
   def monthly_line_item
     MonthlyLineItem.new(subscription)
   end
 
+  def repos
+    subscribed_repos.order(:name)
+  end
+
+  def subscription
+    user.payment_gateway_subscription
+  end
+
   def total_monthly_cost
-    number_to_currency(
-      monthly_line_item.subtotal_in_dollars,
-      precision: 0
-    )
+    monthly_line_item.subtotal_in_dollars
+  end
+
+  private
+
+  attr_reader :user
+
+  def subscribed_repos
+    user.subscribed_repos
   end
 end
--- spec/presenters/account_page_spec.rb
+++ spec/presenters/account_page_spec.rb
@@ -1,36 +1,77 @@
 require "rails_helper"
 
 RSpec.describe AccountPage do
+  describe "#billable_email" do
+    it "returns the user's billable email" do
+      billable_email = "somebody@example.com"
+      user = double("User")
+      page = AccountPage.new(user)
+
+      allow(user).to receive(:billable_email).once.with(no_args).
+        and_return(billable_email)
+
+      expect(page.billable_email).to eq billable_email
+    end
+  end
+
   describe "#monthly_line_item" do
     it "returns the subscription as a monthly line item" do
+      user = create(:user)
       subscription = instance_double(PaymentGatewaySubscription)
-      page = AccountPage.new(
-        billable_email: "somebody@example.com",
-        subscription: subscription,
-        repos: [create(:repo)],
-      )
+      page = AccountPage.new(user)
+
+      allow(user).to receive(:payment_gateway_subscription).once.with(no_args).
+        and_return(subscription)
 
       expect(page.monthly_line_item).to eq MonthlyLineItem.new(subscription)
     end
   end
 
-  describe "#total_monthly_cost" do
-    it "returns the subtotal of the line item in dollars" do
-      coupon = double(:coupon, valid: false)
-      discount = double(:discount, coupon: coupon)
-      subscription = instance_double(
-        PaymentGatewaySubscription,
-        quantity: 1,
-        plan_amount: 200_00,
-        discount: discount,
-      )
-      page = AccountPage.new(
-        billable_email: "somebody@example.com",
-        subscription: subscription,
-        repos: [create(:repo)],
-      )
+  describe "#repos" do
+    it "returns the subscriped repos ordered by name" do
+      ordered_repos = double("Repo")
+      subscribed_repos = double("Repo")
+      user = double("User")
+      page = AccountPage.new(user)
 
-      expect(page.total_monthly_cost).to eq("$200")
+      allow(subscribed_repos).to receive(:order).once.with(:name).
+        and_return(ordered_repos)
+      allow(user).to receive(:subscribed_repos).once.with(no_args).
+        and_return(subscribed_repos)
+
+      expect(page.repos).to eq ordered_repos
+    end
+  end
+
+  describe "#subscription" do
+    it "returns the user's payment gateway subscription" do
+      subscription = double("PaymentGatewaySubscription")
+      user = double("User")
+      page = AccountPage.new(user)
+
+      allow(user).to receive(:payment_gateway_subscription).once.with(no_args).
+        and_return(subscription)
+
+      expect(page.subscription).to eq subscription
+    end
+  end
+
+  describe "#total_monthly_cost" do
+    it "returns the subtotal of the monthly line item in dollars" do
+      cost = 200.00
+      item = instance_double(MonthlyLineItem)
+      subscription = double("PaymentGatewaySubscription")
+      user = double("User")
+      page = AccountPage.new(user)
+
+      allow(MonthlyLineItem).to receive(:new).once.with(subscription).
+        and_return(item)
+      allow(item).to receive(:subtotal_in_dollars).once.with(no_args).
+        and_return(cost)
+      allow(user).to receive(:payment_gateway_subscription).once.with(no_args).
+        and_return(subscription)
+
+      expect(page.total_monthly_cost).to eq cost
     end
   end
 end
